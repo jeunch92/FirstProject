48cc31f2d126ed2ba1175ca93ae1b63dce0d025e
euncheol.jeong <jeunch92@naver.com>
2018-08-16 11:22:41 +0900

    Test.java modified Hello -> Heo

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 class Test{
-	public static void main(String [] args){
-		System.out.println("Hello Git");
+	
+public static void main(String [] args){
+		System.out.println("Heo Git");
 		System.out.println("Hi Git");
 	}
 
